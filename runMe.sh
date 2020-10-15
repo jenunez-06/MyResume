6473e5c10264f6856bbda2d858f0be3575bd5c58
Josue Nunez <jenunez06@gmail.com>
2020-10-15 11:32:24 -0500

    Added copied all the files from ssg-bash branch over to this one. Getting it ready for python scripting assignment

---
+++ runMe.sh
@@ -0,0 +1,14 @@
+
+
+#This creates the home page index.html
+cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+
+#this creates the contact page
+cat templates/top.html content/contact.html templates/bottom.html > docs/contact.html
+
+#this crated the portfolio page
+cat templates/top.html content/portfolio.html templates/bottom.html > docs/portfolio.html
+
+#creates the proExperience page
+cat templates/top.html content/proExperience.html templates/bottom.html > docs/proExperience.html
+
